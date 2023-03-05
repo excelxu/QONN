115a7c8d77b8a74879d11ff78d77002e5132bb8f
excelxu <xu_zhuoyue@mail.ustc.edu.cn>
2023-03-05 22:38:54 +0800

    4-2网络(含辅助量子比特)电路(测量第三比特的layer_42_on_3)与测量第四比特的(layer_42_on_4)

--- qonn_42.py
+++ qonn_42.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import random
 import matplotlib.pyplot as plt
 
-dev = qml.device('default.qubit', wires=[0,1,2,3,'aux']) # 定义量子模拟器
+dev = qml.device('default.qubit', wires=[0,1,2,3,'aux']) # 定义量子模拟器,包含自动微分模式
 
 def RBS(on_wire, theta):
     if type(on_wire) is tuple:
@@ -24,24 +24,31 @@ def RBS(on_wire, theta):
 
 def data_to_state(data):
     # 设定计算基的矢量模式
-    basis10000 = basis01000 = basis00100 =  basis00010 = basis00001 = np.zeros(2**5)
+    basis10000 = np.zeros(2**5)
+    basis01000 = np.zeros(2**5)
+    basis00100 = np.zeros(2**5)
+    basis00010 = np.zeros(2**5)
+    basis00001 = np.zeros(2**5)
     basis10000[2**4] = 1.0
     basis01000[2**3] = 1.0
     basis00100[2**2] = 1.0
     basis00010[2**1] = 1.0
     basis00001[2**0] = 1.0
-    state =  data[0] * basis10000 \
-            +data[1] * basis01000 \
-            +data[2] * basis00100 \
-            +data[3] * basis00010 \
-            +data[4] * basis00001 # 第五位是辅助量子比特上的振幅，一般为0.2
+    state =  data[0]*basis10000\
+            +data[1]*basis01000\
+            +data[2]*basis00100\
+            +data[3]*basis00010\
+            +data[4]*basis00001 # 第五位是辅助量子比特上的振幅，一般为0.2
     return state
 
-@qml.qnode(dev)
-def layer_42(in_state, params):
+# 为了更好地使用qml,初始化量子态必须
+# (1)通过生成电路生成
+# (2)作为全局变量导入
+@qml.qnode(dev,  diff_method="parameter-shift")
+def layer_42_on_3(params):
     # 装载初始数据
     # 拥有五条线路，最下面一条是辅助量子比特
-    qml.QubitStateVector(in_state
+    qml.QubitStateVector(ini_state
                         ,wires=[0,1,2,3,'aux'])
     # 金字塔电路主体
     RBS((0, 1), params[0])
@@ -53,17 +60,53 @@ def layer_42(in_state, params):
     RBS((2, 3), params[3])
     qml.Snapshot("zeta_3")
     RBS((1, 2), params[4])
-    return qml.state()
+    return qml.expval(qml.PauliZ(2))
+
+@qml.qnode(dev,  diff_method="parameter-shift")
+def layer_42_on_4(params):
+    # 装载初始数据
+    # 拥有五条线路，最下面一条是辅助量子比特
+    qml.QubitStateVector(ini_state
+                        ,wires=[0,1,2,3,'aux'])
+    # 金字塔电路主体
+    RBS((0, 1), params[0])
+    qml.Snapshot("zeta_0")
+    RBS((1, 2), params[1])
+    qml.Snapshot("zeta_1")
+    RBS((0, 1), params[2])
+    qml.Snapshot("zeta_2")
+    RBS((2, 3), params[3])
+    qml.Snapshot("zeta_3")
+    RBS((1, 2), params[4])
+    return qml.expval(qml.PauliZ(3))
 
 
 if __name__ == '__main__':
 
     # 测试🐔
-    ini_state = np.zeros(2**5)
-    ini_state[0] = 1.
-    s = layer_42(ini_state, [0,0,0,0,0])
-    # print(s)
-    print(qml.draw(layer_42)(ini_state, [0,0,0,0,0]))
+    # 用于导入量子态数据的全局变量
+    global ini_state
+    # 读取excel中数据
+
+
+
+
+
+    ini_state = data_to_state([0, 0.8, 0.6, 0, 0])
+    print(ini_state)
+    print(qml.draw(layer_42_on_3)([0,0,0,0,0]))
+    print(layer_42_on_3(np.array([0,0,0,0,0])))
+    grad = qml.gradients.param_shift(layer_42_on_3)(np.array([1,0.1,3,3,0]))
+    param = np.array([1, 0.1, 3, 3, 0])
+    print(grad)
+    for i in range(1000):
+        print(layer_42_on_3(param))
+        grad = qml.gradients.param_shift(layer_42_on_3)(param)
+        param = param - grad* 0.01
+
+
+
+
 
 
 
