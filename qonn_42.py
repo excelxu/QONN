6448dc92ac0e64cd28532fe9be00dfd2b87db5ec
excelXu <xu_zhuoyue@mail.ustc.edu.cn>
2023-03-06 22:07:53 +0800

    量子电路对于观测量的期望求梯度。

--- qonn_42.py
+++ qonn_42.py
@@ -7,6 +7,11 @@ from sklearn.model_selection import train_test_split
 import random
 import matplotlib.pyplot as plt
 
+def Standard(x):
+    """Z-score normaliaztion"""
+    x = (x - np.mean(x)) / np.std(x)
+    return x
+
 dev = qml.device('default.mixed', wires=[0,1,2,3,'aux']) # 定义量子模拟器,包含自动微分模式
 
 def RBS(on_wire, theta):
@@ -91,32 +96,28 @@ if __name__ == '__main__':
     bc_train = pd.read_excel('IrisData/bc20_train_100.xlsx')
     ab_test = pd.read_excel('IrisData/irisAB_test_80.xlsx')
     bc_test = pd.read_excel('IrisData/irisBC_test_80.xlsx')
-
-    print(ab_train)
     # 量子网络的初始化
+
+    ini_state = data_to_state([0, 0.8, 0.6, 0, 0])
     param = np.array([1, 0.1, 3, 3, 0]) # 初始化参数
-    print(qml.draw(layer_42_on_3)(param)) # 绘制网络框架
+    lr = 0.1 # 学习率
 
-    for i in range(1):
-        ini_state = data_to_state([0, 0.8, 0.6, 0, 0])  # 自定义的初始态
-        print(layer_42_on_3(param))
-        grad = qml.gradients.param_shift(layer_42_on_3)(param)
-        param = param - grad* 0.2
-
-    print(param)
-
-
-
-
-
-
-
-# # 确定计算基底的矢量形式
-#     @qml.qnode(dev)
-#     def basis():
-#         qml.PauliX(wires= 2 )
-#         return qml.state()
-#     s = basis()
-#     print(s)
+    for i in range(100): # 遍历一次训练集（100个）
+        # 每次10次对梯度做一次升级
+        data = ab_train.values[i,(0,1,2,3)]
+        data = Standard(data)
+        feat = np.sqrt(0.8)*(data)/np.sqrt(np.dot(data,data))
+        f = [feat[0], feat[1], feat[2], feat[3], np.sqrt(0.2)]
+        data = f # 输入态
+        label = ab_train.values[i,4] #标签
+        #---量子电路的输出与梯度
+        ini_state = data_to_state(data)
+        out3 = layer_42_on_3(param)
+        out4 = layer_42_on_4(param)
+        grad3 = qml.gradients.param_shift(layer_42_on_3)(param)
+        grad4 = qml.gradients.param_shift(layer_42_on_4)(param)
+        # 计算观测量out3，out4与概率的关系，并且转化为振幅
+        print(out3)
+        param = param - grad3 * 0.2
 
 
